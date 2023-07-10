eb46c2de8a624c181b9cde952943edf3ce31dbb7
NangKo3078 <gusals3078@naver.com>
2023-07-10 17:05:08 +0900

    [23.07.10] README 수정

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/react-in-jsx-scope */
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StatusBar, View } from 'react-native';
 import SvgIconAtom from './src/components/atoms/SvgIcon.atom';
 
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1, padding: 24, backgroundColor: 'white' }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        padding: 24,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <StatusBar barStyle={'dark-content'} />
       {/* ... */}
-      <SvgIconAtom name={'HomeBlack'} />
-      <SvgIconAtom name={'MenuBlack'} fill={'orange'} />
+      <SvgIconAtom name={'HomeBlack'} size={30} />
+      <View style={{ marginTop: 20 }}>
+        <SvgIconAtom name={'MenuBlack'} fill={'red'} />
+      </View>
       {/* ... */}
     </SafeAreaView>
   );
